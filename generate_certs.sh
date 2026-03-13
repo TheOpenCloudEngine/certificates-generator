99d6447d425999504a922b98e7f175aba4d63012
Edward KIM <fharenheit@gmail.com>
2026-03-13 11:12:05 +0900

    Update generate_certs.sh

--- generate_certs.sh
+++ generate_certs.sh
@@ -28,8 +28,8 @@ ORGANIZATION="Open Cloud Engine Community"  # O  - 조직명
 ORG_UNIT="Platform Engineering"             # OU - 부서명
 
 # 도메인 정보
-DOMAIN="opencloudengine.org"                # 호스트명에 붙는 기본 도메인
-WILDCARD="*.${DOMAIN}"            # 와일드카드 도메인
+DOMAIN="opencloudengine.org"        # 호스트명에 붙는 기본 도메인
+WILDCARD="*.${DOMAIN}"              # 와일드카드 도메인
 
 # CA 설정
 CA_CN="Open Cloud Engine Community CA"      # CA의 Common Name
